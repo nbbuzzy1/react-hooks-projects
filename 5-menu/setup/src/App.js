955a36e69f139f1d6ec1dbc118d582b7a8029b7d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2021-01-26 14:47:07 -0500

    Restaurant menu.

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,8 +3,34 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  return <h2>menu project setup</h2>;
+const allCategories = ['all', ...new Set(items.map(({ category }) => category))];
+
+const App = () => {
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories, setCategories] = useState(allCategories);
+	console.log(allCategories);
+	const filterItems = (category) => {
+		if (category === 'all') {
+			setMenuItems(items);
+			return;
+		}
+
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	}
+
+	return (
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>Our Menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories filterItems={filterItems} categories={categories} />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
 }
 
 export default App;
