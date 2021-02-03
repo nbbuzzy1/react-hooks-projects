5514baeb5b002e1ae0fe60474d79b941fc875194
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2021-02-03 13:16:16 -0500

    Sidebar, modal components using useContext.

--- 12-sidebar-modal/setup/src/App.js
+++ 12-sidebar-modal/setup/src/App.js
@@ -3,11 +3,13 @@ import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
 function App() {
-  return (
-    <>
-      <h2>modal-sidebar project setup</h2>
-    </>
-  )
+	return (
+		<>
+			<Home />
+			<Modal />
+			<Sidebar />
+		</>
+	)
 }
 
 export default App
