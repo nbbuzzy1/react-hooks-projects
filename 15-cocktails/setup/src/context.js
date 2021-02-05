208c61a885658a8d78bbcbe96fef8b5b71783bfb
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2021-02-05 17:24:45 -0500

    Finish cocktails project.

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
 	const [searchTerm, setSearchTerm] = useState('a');
 	const [cocktails, setCocktails] = useState([]);
 
-	const fetchDrinks = async () => {
+	const fetchDrinks = useCallback(async () => {
 		setLoading(true);
 
 		try {
@@ -24,11 +24,11 @@ const AppProvider = ({ children }) => {
 		} finally {
 			setLoading(false);
 		}
-	}
+	}, [searchTerm])
 
 	useEffect(() => {
 		fetchDrinks();
-	}, [searchTerm])
+	}, [searchTerm, fetchDrinks])
 
 	return <AppContext.Provider value={{
 		loading,
